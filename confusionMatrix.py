749ace69a3c0bdd998940227a1a9ae65a64cdbf3
Apoorva-Prashanth <99094642+Apoorva-Prashanth@users.noreply.github.com>
2025-05-20 23:42:09 +0530

    Update confusionMatrix.py

--- confusionMatrix.py
+++ confusionMatrix.py
@@ -1,54 +1,13 @@
-"""import pandas as pd
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-import matplotlib.pyplot as plt
-from matplotlib import font_manager
-
-# Load CSV
-df = pd.read_csv("C:/Users/APOORVA/Downloads/predictions_vanilla - test_predictions_embed256_hid128_enc2_dec1_LSTM_drop0.35_beam5.csv")
-
-# Load custom Malayalam font
-font_path = "C:/Users/APOORVA/Downloads/Noto_Sans_Malayalam/NotoSansMalayalam-VariableFont_wdth,wght.ttf"
-mal_font = font_manager.FontProperties(fname=font_path)
-
-# Extract characters
-true_chars = []
-pred_chars = []
-
-for true, pred in zip(df["True Native"], df["Predicted Native"]):
-    min_len = min(len(true), len(pred))
-    for i in range(min_len):
-        true_chars.append(true[i])
-        pred_chars.append(pred[i])
-
-# Create confusion matrix
-labels = sorted(set(true_chars + pred_chars))
-cm = confusion_matrix(true_chars, pred_chars, labels=labels)
-
-# Plot confusion matrix manually (with Malayalam font)
-fig, ax = plt.subplots(figsize=(7, 7))
-im = ax.imshow(cm, cmap='Blues')
-
-# Tick labels with Malayalam characters
-ax.set_xticks(range(len(labels)))
-ax.set_yticks(range(len(labels)))
-ax.set_xticklabels(labels, fontproperties=mal_font, fontsize=10, rotation=90)
-ax.set_yticklabels(labels, fontproperties=mal_font, fontsize=10)
-
-# Title and color bar
-ax.set_title("Character-Level Confusion Matrix (Malayalam)", fontproperties=mal_font, fontsize=16)
-plt.colorbar(im, ax=ax, fraction=0.046, pad=0.04)
-plt.tight_layout()
-plt.show()"""
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 
 # Load CSV
-df = pd.read_csv("C:/Users/APOORVA/Downloads/predictions_vanilla - test_predictions_embed256_hid128_enc2_dec1_LSTM_drop0.35_beam5.csv")
+df = pd.read_csv("C:/Users/APOORVA/Downloads/predictions_vanilla - test_predictions_embed256_hid128_enc2_dec1_LSTM_drop0.35_beam5.csv")  #submitted in the repo
 
 # Load custom Malayalam font
-font_path = "C:/Users/APOORVA/Downloads/Noto_Sans_Malayalam/NotoSansMalayalam-VariableFont_wdth,wght.ttf"
+font_path = "C:/Users/APOORVA/Downloads/Noto_Sans_Malayalam/NotoSansMalayalam-VariableFont_wdth,wght.ttf"  #download malayalam font from Google and specify the local path
 mal_font = font_manager.FontProperties(fname=font_path)
 
 # Extract characters
